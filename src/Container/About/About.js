ee06e18412a882910272f5c732d32d3aef55d960
ChumaMqeke <cmqeke2401@gmail.com>
2024-02-09 16:41:22 +0200

    About content

--- src/Container/About/About.js
+++ src/Container/About/About.js
@@ -6,12 +6,37 @@ export default function About() {
   return (
     <div className='about-page'>
       <div className='about-container'>
+      <div class="spinner">
+    <div class="spinner1"> </div>
+</div>
         <div className='left-box'>
+        <div className='highlight-box'>
+              <ul className='highlight-text'>
+                <h4 className='heading'><span className='about-line'>Few</span> Highlights About <span className='text-style'>Me</span></h4>
+                <p>
+                Proficient in developing dynamic and responsive websites.
+                </p>
+                <p>
+                Creates stunning and intuitive user interfaces using Figma.
+                </p>
+                <p>
+                Expertise in utilizing React as a robust framework for web development.
+                </p>
+                <p>
+                Continuous Learner and Tech Enthusiast
+                 Passionate Problem Solver
+                </p>
+              </ul>
+            </div>
+
+
           <div className='about-text'>
             <h2><span className='about-line'>About</span> <span className='text-style'>Me</span></h2>
           </div>
           <div className='top-text'>
             <img className='profile-picture' src={figmaProfile} alt="Description of the image" />
+           
+            
             <div className='rectangle-box'>
               <p className='text'>
                 I am a junior software developer with a focus on front-end and back-end development,
