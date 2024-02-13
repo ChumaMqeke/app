7a2d2b40f3be71ce2558d89f9e44ca10b3266aa7
ChumaMqeke <cmqeke2401@gmail.com>
2024-02-13 16:07:15 +0200

    Skills page

--- src/Container/Skills/Skills.js
+++ src/Container/Skills/Skills.js
@@ -1,18 +1,86 @@
-import React from 'react'
+import React from 'react';
 import './Skills.css';
+
 export default function Skills() {
   return (
     <div className='skills-container'>
-        <div className='skills-cover'>
-        <div id="cont">
-         <div id="box"></div>
+      <div className='skills-text'>
+        <h2><span className='text-style'>My</span> S<span className='about-line'>kills</span></h2>
+      </div>
+      <div className='skills-cover'>
 
-                <div className='skills-circle'>
-
-                </div>
+        <div className='rows'>
+          <div className='square'>
+            <div id="cont">
+              <div id="box"><i className="fa-brands fa-html5"></i></div>
             </div>
+            <div className="progress-bar-container">
+              <div className="progress-bar" style={{ width: '80%' }}>
+                <div className="progress-text">80%</div>
+              </div>
+            </div>
+          </div>
+
+          <div className='square'>
+            <div id="cont">
+              <div id="box"><i className="fa-brands fa-css3"></i></div>
+            </div>
+            <div className="progress-bar-container">
+              <div className="progress-bar" style={{ width: '90%' }}>
+                <div className="progress-text">90%</div>
+              </div>
+            </div>
+          </div>
         </div>
 
+        <div className='rows'>
+          <div className='square'>
+            <div id="cont">
+              <div id="box"><i className="fa-brands fa-js"></i></div>
+            </div>
+            <div className="progress-bar-container">
+              <div className="progress-bar" style={{ width: '75%' }}>
+                <div className="progress-text">75%</div>
+              </div>
+            </div>
+          </div>
+
+          <div className='square'>
+            <div id="cont">
+              <div id="box"><i className="fa-brands fa-react"></i></div>
+            </div>
+            <div className="progress-bar-container">
+              <div className="progress-bar" style={{ width: '60%' }}>
+                <div className="progress-text">60%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='rows'>
+          <div className='square'>
+            <div id="cont">
+              <div id="box"><i className="fa-brands fa-java"></i></div>
+            </div>
+            <div className="progress-bar-container">
+              <div className="progress-bar" style={{ width: '50%' }}>
+                <div className="progress-text">65%</div>
+              </div>
+            </div>
+          </div>
+
+          <div className='square'>
+            <div id="cont">
+              <div id="box"><i className="fa-brands fa-python"></i></div>
+            </div>
+            <div className="progress-bar-container">
+              <div className="progress-bar" style={{ width: '50%' }}>
+                <div className="progress-text">50%</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
